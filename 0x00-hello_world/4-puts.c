629f6ae54c7f1118dacc5f197f36769f19adf317
IVashty <vashtylavilavi50@gmail.com>
2022-06-30 15:14:54 -0700

    Task 4 update 3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+/*
+ * main-shld print a string.
+ * Description:use puts
+ * Return:0
+ */
 int main(void)
+	/**
+	 * main acts as Entry point
+	 * Description: it should look like puts("string")
+	 * Return:0
+	 */
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * art_words - Print a string
+ * Description: use printf
+ * Return: 0
+ */
+
+int art_words(void)
+{
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
