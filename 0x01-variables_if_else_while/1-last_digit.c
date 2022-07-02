a73145319f9ccb05882dec1fe8cf616283fa7894
Cody McCodePants <cody@mccodepants.com>
2022-07-02 14:03:50 -0700

    Task 1 update 7

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,6 +22,6 @@ printf("Last digit of %d is %d and is greater than 5\n", n, v);
 else if (n != 0 && n < 6)
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, v);
 else
-printf("Last digit of %d is %d and is zero\n", n, v);
+printf("Last digit of %d is %d and is 0\n", n, v);
 return (0);
 }
