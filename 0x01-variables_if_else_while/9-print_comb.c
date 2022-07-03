e9bd5b811dc884463b172848b6c23818017867b5
Cody McCodePants <cody@mccodepants.com>
2022-07-03 04:34:43 -0700

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main block of code
+ *Description:a program that prints a comination of single digits all separated
+ * by commas,followed by a new line using the putchar function .
+ * Return:0(Success)
+ */
+int main(void)
+{
+int v;
+for (v = '0'; v <= '9'; v++)
+{
+putchar(v + ',');
+}
+putchar('\n');
+
+return (0);
+}
