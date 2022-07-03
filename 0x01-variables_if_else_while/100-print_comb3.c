d2685525e443374355e9a2642b1afdd492c2d58e
Cody McCodePants <cody@mccodepants.com>
2022-07-03 05:15:53 -0700

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - main block of code
+ *Description:a program that prints a combination of two digits all
+ *separated by commas,followed by a new line using the putchar function .
+ * Return:0(Success)
+ */
+int main(void)
+{
+int v;
+int o;
+for (v = '0'; v <= '9'; v++)
+{
+for (o = '1'; o <= '9'; o++)
+{
+if (v > o)
+{
+putchar(v);
+putchar(o);
+
+if (v != 8 || 0 != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+
+return (0);
+}
