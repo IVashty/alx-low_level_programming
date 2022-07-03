f3e4697c8322306a68f10b2fc7e4440de8583e59
Cody McCodePants <cody@mccodepants.com>
2022-07-03 03:31:08 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main block of code
+ * Description:a program that prints the alphabet in lowercase,
+ * *followed by a new line using the putchar function .
+ * Return:0(Success)
+ */
+int main(void)
+{
+int v;
+for (v = 0; v <= 10; v++)
+{
+printf("%d\n", v);
+}
+
+return (0);
+}
