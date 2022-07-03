9fa5833b3c9ac4d2b4458a6332604f173f10d162
Cody McCodePants <cody@mccodepants.com>
2022-07-03 06:11:07 -0700

    Task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+#include <stdio.h>
+/**
+ * main - main block of code
+ *Description:a program that prints a combination of three digits all
+ *separated by commas,followed by a new line using the putchar function .
+ * Return:0(Success)
+ */
+int main(void)
+{
+int v;
+int o;
+int t;
+for (v = '0'; v <= '9'; v++)
+{
+for (o = '1'; o <= '9'; o++)
+{
+for (t = '2'; t <= '9'; t++)
+{
+if (o > v && t > v && t > o)
+{
+putchar(v + o);
+putchar(' ');
+putchar(o + t);
+if (v != '7' || o != '8' || t != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+{
+putchar('\n');
+}
+return (0);
+}
