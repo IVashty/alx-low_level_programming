678425e034087712f95fb28e86ac0a8a6083b4ec
Cody McCodePants <cody@mccodepants.com>
2022-07-03 08:27:35 -0700

    Task 12 update 6

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,17 +11,17 @@ int main(void)
 {
 int v;
 int o;
-for (v = 0; v <= 100; v++)
+for (v = 0; v < 100; v++)
 {
-for (o = 0; o <= 100; o++)
+for (o = 0; o < 100; o++)
 {
 if (v < o)
 {
-putchar(v / 10 + '0');
-putchar(v % 10 + '0');
+putchar((v / 10) + 0);
+putchar((v % 10) + 0);
 putchar(' ');
-putchar(o / 10 + '0');
-putchar(o % 10 + '0');
+putchar((o / 10) + 0);
+putchar((o % 10) + 0);
 if (v != 98 || o != 99)
 {
 putchar(',');
