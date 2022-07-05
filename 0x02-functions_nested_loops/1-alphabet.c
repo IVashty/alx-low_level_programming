ac18ff07b22a695268893ec0be4b75d15f492134
Cody McCodePants <cody@mccodepants.com>
2022-07-05 07:43:34 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ ** main - check the code
+ **
+ ** Return: Always 0.
+ **/
+/*
+ * int main(void)
+ */
+void print_alphabet(void)
+{
+int v;
+for (v = 'a'; v <= 'z'; v++)
+{
+_putchar(v);
+}
+_putchar('\n');
+/*
+ * return (0);
+ */
+}
