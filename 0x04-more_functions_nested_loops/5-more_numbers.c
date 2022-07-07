75daac7b6b696d6cdafacb04ff36be0bc744ef45
Cody McCodePants <cody@mccodepants.com>
2022-07-07 13:49:03 -0700

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - function prints numbers from 0 to 15 ten times
+ * Return:0 is success
+ */
+
+void more_numbers(void)
+{
+int v;
+v = 0;
+while (v < 15)
+{
+if (v < 15)
+_putchar(v / 10  + '0');
+_putchar(v % 10 + '0');
+v++;
+}
+_putchar('\n');
+}
+
