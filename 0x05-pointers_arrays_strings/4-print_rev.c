4a8b719d522cc134b7cacc6676d61e97eb8e71a7
Cody McCodePants <cody@mccodepants.com>
2022-07-12 01:37:51 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev- a function that prints a string in reverse.
+ * followed by a new line.
+ * @s:char array string type
+ * Description:only use _putchar.
+ * Return: 0 always
+ */
+
+void print_rev(char *s)
+{
+int v;
+
+for (v = 0; s[v] != '\0'; v++)
+{
+_putchar(s[v]);
+}
+for (v--; v >= 0; v--)
+{
+_putchar(s[v]);
+}
+
+_putchar('\n');
+}
