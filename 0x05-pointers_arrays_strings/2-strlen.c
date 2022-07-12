2dd47a60c229f36dcb76b9c3df8a010eff98a08c
Cody McCodePants <cody@mccodepants.com>
2022-07-12 01:25:08 -0700

    Task 2 update 4

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+#include <string.h>
 
 /**
  * _strlen - funtion returns the length of a string
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,6 +3,7 @@
 
 int _putchar(char c);
 int main(void);
+int _strlen(char *);
 
 
 void reset_to_98(int *n);
