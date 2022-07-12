3d29a7ae83f8ef9aa6b73cf054c6276494195c47
Cody McCodePants <cody@mccodepants.com>
2022-07-12 06:52:40 -0700

    Task 5 update 3

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -13,18 +13,22 @@ int a;
 char str;
 int f;
 
+v = 0;
 for (v = 0; s[v] != '\0'; v++)
 ;
 
-a = v;
+a = v - 1;
 r = 0;
 f = a / 2;
+
+
 for (v--; r < f; r++)
 ;
 {
 str = s[r];
 s[r] = s[v];
 s[v] = str;
+a -= 1;
 }
 
 _putchar('\n');
