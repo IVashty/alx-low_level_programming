02b75dfeeaa6dc62bcf35bd1a128996c5c72fd73
Cody McCodePants <cody@mccodepants.com>
2022-07-12 06:06:33 -0700

    Task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,4 +9,19 @@ void rev_string(char *s)
 {
 int v;
 int r;
+int a;
+char str;
+
+for (v = 0; s[v] != '\0'; v++)
+;
+
+a = v;
+for (v--, r = 0; r < a / 2; r++)
+{
+str = s[r];
+s[r] = s[v];
+s[v] = str;
+}
+
+_putchar('\n');
 }
