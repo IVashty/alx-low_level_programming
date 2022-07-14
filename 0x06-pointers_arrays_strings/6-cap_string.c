84e50755282e641078cafc8b103b25f3c8013688
Cody McCodePants <cody@mccodepants.com>
2022-07-14 07:04:21 -0700

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,31 +1,31 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * *cap_string-a function that capitalises all word of a string.
- * @char f:a string type pointer
- * Return:a char
+ * @f:a string type pointer
+ * Return:f
  */
 
 char *cap_string(char *f)
 {
 int v;
-int a;
-int ch;
+int len;
 char specs[] = {32, 9, '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
 
 v = 0;
+len = sizeof(specs) / sizeof(specs[0]);
 while (f[v])
-v++;
+
 {
-while (f[v] >= 'a' && f[v] <= 'z')
+while (v < len)
 {
-if (f[v - 1]  == f[specs] && v == 0)
-{
+if ((v == 0 || f[v - 1]  == specs[v]) && (f[v] >= 'a' && f[v] <= 'z'))
 f[v] -= 32;
 }
 v++;
 }
-}
+
 
 return (f);
 }
