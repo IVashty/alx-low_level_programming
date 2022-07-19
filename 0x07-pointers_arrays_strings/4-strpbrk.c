f034955496259116ccb931bd56243077eb64d176
Cody McCodePants <cody@mccodepants.com>
2022-07-19 01:05:45 -0700

    Task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,5 +9,26 @@
 
 char *_strpbrk(char *s, char *accept)
 {
+int v;
+int j;
 
+v = 0;
+
+while (s[v])
+{
+j = 0;
+while (accept[j])
+{
+if (accept[j] == s[v])
+{
+s += v;
+return (s);
+}
+j++;
+
+}
+v++;
+}
+
+return ('\0');
 }
