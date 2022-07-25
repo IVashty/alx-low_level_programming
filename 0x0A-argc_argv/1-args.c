469a4e4cca8766c04e2c419c97a8e4f1f6219515
Cody McCodePants <cody@mccodepants.com>
2022-07-25 12:18:57 -0700

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main-function that prints its name followed by a newline.
+ * @argc:integer type of value
+ * @argv:char type of pointer
+ * Return:0
+ */
+
+int main(int argc, char *argv[])
+{
+(void) argv;
+
+printf("%d\n", argc - 1);
+return (0);
+}
