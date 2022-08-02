f8e23c16542e2b249cd7d5d51e6383128d89b0f6
Cody McCodePants <cody@mccodepants.com>
2022-08-02 11:14:56 -0700

    Task 5

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog -function that creates a new dog.
+ * @name:character  string type
+ * @age:integer in float form
+ * @owner:character string type
+ * Return:pointer to new_dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+}
