6e8dc53a15e547772f453ef906ba14dacc9a3cd6
Cody McCodePants <cody@mccodepants.com>
2022-08-02 04:05:02 -0700

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DEFINE_H
+
+/**
+ * struct dog - structure definition of a dog.
+ * @name:character string
+ * @age:integer
+ * @owner:character string
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+/**
+ * dog_t-typedef for struct dog
+ */
+
+typedef struct dog dog_t;
