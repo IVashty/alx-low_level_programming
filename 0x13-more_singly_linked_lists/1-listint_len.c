714c892a308cac9a360515858b39dc98b6f1a998
IVashty <vashtylavilavi50@gmail.com>
2022-08-16 13:37:07 +0000

    Task 1-update 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,6 +3,7 @@
 /**
  * listint_len-fn  that returns the number of elements linked listint_t list.
  * @h:pointer variable of linked list listint_len
+ * Return:number of elements.
  */
 
 size_t listint_len(const listint_t *h)
