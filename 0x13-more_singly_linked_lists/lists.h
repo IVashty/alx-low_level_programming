e42e6fe6bc3c0020d797682db8bf7d451fc396e7
IVashty <vashtylavilavi50@gmail.com>
2022-08-17 12:15:07 +0000

    Task 12

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -60,7 +60,8 @@ listint_t *reverse_listint(listint_t **head);
 /*101-print_listint_safe function*/
 size_t print_listint_safe(const listint_t *head);
 
-
+/*102-free_listint_safe*/
+size_t free_listint_safe(listint_t **h);
 
 
 
